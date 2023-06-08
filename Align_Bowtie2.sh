65110baaf7dda46b4f488e5706725cd00a21100f
Harihara Subrahmaniam Muralidharan <42724690+hsmurali@users.noreply.github.com>
2023-06-08 19:22:23 -0400

    Add files via upload

---
+++ Align_Bowtie2.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#SBATCH -J BowTie2 # Job name
+#SBATCH -o Bowtie2.o # Name of output file
+#SBATCH -e Bowtie2.e # Name of error file
+#SBATCH --mail-user=hsmurali@terpmail.umd.edu # Email for job info
+#SBATCH --mail-type=all # Get email for begin, end, and fail
+#SBATCH --time=01:00:00
+#SBATCH --partition=cbcb 
+#SBATCH --account=cbcb 
+#SBATCH --ntasks=16
+#SBATCH --qos=high
+#SBATCH --mem=32gb
+
+module load bowtie2/2.4.1
+module load samtools/1.16
+
+READS_1=/fs/cbcb-scratch/hsmurali/REU_2023/Reads/MET0319_R1.fastq.gz
+READS_2=/fs/cbcb-scratch/hsmurali/REU_2023/Reads/MET0319_R2.fastq.gz
+REF=/fs/cbcb-scratch/hsmurali/REU_2023/GCA_000025765.1_ASM2576v1_genomic.fna
+OUT_DIR=/fs/cbcb-scratch/hsmurali/REU_2023_Test/
+IDX=${OUT_DIR}GCA_000025765.1_ASM2576v1_genomic.idx
+
+##BUILD A BOWTIE2 INDEX
+bowtie2-build ${REF} ${IDX}
+
+#ALIGN READS TO REFERENCE
+/usr/bin/time -v bowtie2 -p 16 -x ${IDX} -U ${READS_1},${READS_2} --no-unal | samtools view -bS > ${OUT_DIR}/alignments.bam
---
+++ Align_Minimap2.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH -J Minimap2 # Job name
+#SBATCH -o Minimap2.o # Name of output file
+#SBATCH -e Minimap2.e # Name of error file
+#SBATCH --mail-user=hsmurali@terpmail.umd.edu # Email for job info
+#SBATCH --mail-type=all # Get email for begin, end, and fail
+#SBATCH --time=01:00:00
+#SBATCH --partition=cbcb 
+#SBATCH --account=cbcb 
+#SBATCH --ntasks=16
+#SBATCH --qos=high
+#SBATCH --mem=32gb
+
+module load conda
+source activate /fs/cbcb-software/RedHat-7-x86_64/users/hsmurali/venvs/Minimap2
+
+READS_1=/fs/cbcb-scratch/hsmurali/REU_2023/Reads/MET0319_R1.fastq.gz
+READS_2=/fs/cbcb-scratch/hsmurali/REU_2023/Reads/MET0319_R2.fastq.gz
+REF=/fs/cbcb-scratch/hsmurali/REU_2023/GCA_000025765.1_ASM2576v1_genomic.fna
+OUT_DIR=/fs/cbcb-scratch/hsmurali/REU_2023_Test/
+
+/usr/bin/time -v minimap2 -t 16 ${REF} ${READS_1}  > ${OUT_DIR}/READS_1_Minimap2.paf
+/usr/bin/time -v minimap2 -t 16 ${REF} ${READS_2}  > ${OUT_DIR}/READS_2_Minimap2.paf
